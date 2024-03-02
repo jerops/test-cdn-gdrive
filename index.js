bf5fa65cffb3e9350bb2100ecb00a5138c6d431f
Jeronimo Piquero <jeropsweb@gmail.com>
2024-03-02 17:29:38 +0100

    v1
    set pink form gdrive

---
+++ index.js
@@ -0,0 +1,5 @@
+// Select the first element with the class 'color-black'
+const element = document.querySelector('.color-black');
+
+// Apply a pink background color to it
+element.style.backgroundColor = 'pink';
